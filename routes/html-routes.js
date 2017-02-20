3db44f9962f81c6af4f36c2a4109246700d5e531
alejandroharb <alejandroharb@gmail.com>
2017-02-20 09:37:57 -0600

    added a sample index.html route and api route for maps api

---
+++ routes/html-routes.js
@@ -0,0 +1,10 @@
+//------dependencies-------
+var path = require('path');
+
+
+//-----exporting paths------
+module.exports = function(app) {
+    app.get('/', function(req, res) {
+        res.sendFile(path.join(__dirname, 'index.html'))
+    });
+}
