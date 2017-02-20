19c7de606639d4b6930192b803eaab7323ea0dc6
alejandroharb <alejandroharb@gmail.com>
2017-02-20 16:12:34 -0600

    added googleapi server side npm

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,7 +1,42 @@
 var db = require('../models');
+var NodeGeocoder = require('node-geocoder');
+var geoJSON = require('geojson');
+var options = {
+    provider: 'google'
+}
+var geocoder = NodeGeocoder(options);
+
+var googleMapsClient = require('@google/maps').createClient({
+  key: 'AIzaSyBj7XdOzV55JxV5BBEVg0EZbTXv-Aw3ktk'
+});
+
+//---------temporary hard coded DATA------------
+var data = [
+  { name: 'Houston', category: 'Store', street: 'Market', lat: 29.760, lng: -95.3696 },
+  { name: 'Houston', category: 'Store', street: 'Market', lat: 29.750, lng: -95.3693 },
+  { name: 'Houston', category: 'Store', street: 'Market', lat: 29.7654, lng: -95.3695 },
+  { name: 'Houston', category: 'Store', street: 'Market', lat: 29.7702, lng: -95.36981 },
+  { name: 'Houston', category: 'Store', street: 'Market', lat: 29.75013, lng: -95.36932 },
+  { name: 'Houston', category: 'Store', street: 'Market', lat: 29.76501, lng: -95.36953 },
+  { name: 'Houston', category: 'Store', street: 'Market', lat: 29.7702, lng: -95.369865 },
+  { name: 'Houston', category: 'Store', street: 'Market', lat: 29.7611, lng: -95.36973 },
+  { name: 'Houston', category: 'Store', street: 'Market', lat: 29.7611, lng: -95.36972 },
+  { name: 'Houston', category: 'Store', street: 'Market', lat: 29.761, lng: -95.36971 },
+  { name: 'Houston', category: 'Store', street: 'Market', lat: 29.76121, lng: -95.36971 },
+  { name: 'Houston', category: 'Store', street: 'Market', lat: 29.7612, lng: -95.36971 },
+  { name: 'Houston', category: 'Store', street: 'Market', lat: 29.7611, lng: -95.36971 },
+  { name: 'Houston', category: 'Store', street: 'Market', lat: 29.762, lng: -95.3680 },
+  { name: 'Houston', category: 'Store', street: 'Market', lat: 29.764, lng: -95.3690 },
+];
+
+
+var geoData = geoJSON.parse(data, {Point: ['lat', 'lng']});
+
+// console.log(geoData.features[0].geometry.coordinates);
+var dataArr = geoData.features;
 
 module.exports = function(app) {
     app.get('/api', function(req, res) {
-        res.send(geoData);
+        res.send(dataArr)
     })
 }
--- server.js
+++ server.js
@@ -1,11 +1,7 @@
-var geoJSON = require('geojson');
 var express = require('express');
 var path = require('path');
-var NodeGeocoder = require('node-geocoder');
-var options = {
-	provider: 'google'
-}
-var geocoder = NodeGeocoder(options);
+
+
 var app = express();
 
 
@@ -15,42 +11,19 @@ var db = require('./models')
 
 var PORT = process.env.PORT || 8080;
 
+// Static directory
+app.use(express.static("./public"));
 
 
-var data = [
-  { name: 'Houston', category: 'Store', street: 'Market', lat: 29.760, lng: -95.3696 },
-  { name: 'Houston', category: 'Store', street: 'Market', lat: 29.750, lng: -95.3693 },
-  { name: 'Houston', category: 'Store', street: 'Market', lat: 29.7654, lng: -95.3695 },
-  { name: 'Houston', category: 'Store', street: 'Market', lat: 29.7702, lng: -95.36981 },
-  { name: 'Houston', category: 'Store', street: 'Market', lat: 29.75013, lng: -95.36932 },
-  { name: 'Houston', category: 'Store', street: 'Market', lat: 29.76501, lng: -95.36953 },
-  { name: 'Houston', category: 'Store', street: 'Market', lat: 29.7702, lng: -95.369865 },
-  { name: 'Houston', category: 'Store', street: 'Market', lat: 29.7611, lng: -95.36973 },
-  { name: 'Houston', category: 'Store', street: 'Market', lat: 29.7611, lng: -95.36972 },
-  { name: 'Houston', category: 'Store', street: 'Market', lat: 29.761, lng: -95.36971 },
-  { name: 'Houston', category: 'Store', street: 'Market', lat: 29.76121, lng: -95.36971 },
-  { name: 'Houston', category: 'Store', street: 'Market', lat: 29.7612, lng: -95.36971 },
-  { name: 'Houston', category: 'Store', street: 'Market', lat: 29.7611, lng: -95.36971 },
-  { name: 'Houston', category: 'Store', street: 'Market', lat: 29.762, lng: -95.3680 },
-  { name: 'Houston', category: 'Store', street: 'Market', lat: 29.764, lng: -95.3690 },
-];
-
-var geoData = geoJSON.parse(data, {Point: ['lat', 'lng']});
-
-// console.log(geoData.features);
-var dataArr = geoData.features;
 
 // geocoder.geocode('3918 Atascocita Rd Houston, Texas 77396', function(err, res) {
 // 	console.log(res)
 // })
 
 
-app.get('/', function(req, res) {
-	res.sendFile(path.join(__dirname, 'index.html'))
-})
-app.get('/api', function(req, res) {
-	res.send(geoData);
-})
+//==========importing routes=============
+require('./routes/api-routes.js')(app);
+require('./routes/html-routes.js')(app);
 
 db.sequelize.sync({ force: true }).then(function(){
   app.listen(PORT, function() {
