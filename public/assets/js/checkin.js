d8ff4dd6f3d5151d574ac122e57677eef2efec23
alejandroharb <alejandroharb@gmail.com>
2017-02-22 09:27:35 -0600

    added check-in button functionality with preloader, emptying contents after submiting, and reinitializing functionality

--- public/assets/js/checkin.js
+++ public/assets/js/checkin.js
@@ -1,24 +1,41 @@
 $(document).ready(function() {
     $('#checkInBtn').on('click',function() {
+        //-----initiate preloader-----
+        $('#checkInPreloader').addClass('progress');
         navigator.geolocation.getCurrentPosition(function(position) {
             var pos = {
               lat: position.coords.latitude,
               lng: position.coords.longitude
             };
-            console.log("position from checkin.js")
-            // console.log(pos)
             $.post('/get-places', pos, function(data) {
-                // console.log(data);
-                // var newselect = $('<select>').attr('id','checkinSelect')
-                // $('#checkinDiv').append(newselect);
+                //-----adding options of businesses to select element-----
                 for(var i = 0; i < data.length; i++) {
                     var newoption = $('<option>');
                     newoption.attr('value', data[i].name)
                     newoption.html(data[i].name);
-                    $('#checkinSelect').append(newoption)
+                    $('#checkinSelect').append(newoption);
                 }
+                $('#checkInPreloader').removeClass('progress');
+                //reseting select element to display new dynamically created options
                 $('select').material_select();
+                //--------------------------
+                //add new submit button
+                //add
             })
         });
+    })
+    $('#checkInSubmit').on('click', function() {
+        //-----collect form elements here---
+        //
+        //--ajax post to send data to back-end here
+        //
+        //--------------------
+        //closes the collapsible feature
+        $('#checkInBtn').removeClass('active');
+        $('.collapsible').collapsible();
+        //emptying the dynamic select element, and reinitializing it
+        $('#checkinSelect').empty();
+        $('select').material_select();
+        console.log("ran close function")
     })
 })
